b56eef1cd85275371a6bece9592eaa5b53c78985
VLADoookkk <112398948+VLADoookkk@users.noreply.github.com>
2022-09-26 12:55:28 +0300

    lesson 7 homework 7

---
+++ урок 7/“Сложные запросы”.sql
@@ -0,0 +1,88 @@
+-- 1. Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
+
+INSERT INTO orders (user_id)
+SELECT id FROM users WHERE name = 'Геннадий';
+
+INSERT INTO orders_products (order_id, product_id, total)
+SELECT LAST_INSERT_ID(), id, 2 FROM products
+WHERE name = 'Intel Core i5-7400';
+
+INSERT INTO orders (user_id)
+SELECT id FROM users WHERE name = 'Наталья';
+
+INSERT INTO orders_products (order_id, product_id, total)
+SELECT LAST_INSERT_ID(), id, 1 FROM products
+WHERE name IN ('Intel Core i5-7400', 'Gigabyte H310M S2H');
+
+INSERT INTO orders (user_id)
+SELECT id FROM users WHERE name = 'Иван';
+
+INSERT INTO orders_products (order_id, product_id, total)
+SELECT LAST_INSERT_ID(), id, 1 FROM products
+WHERE name IN ('AMD FX-8320', 'ASUS ROG MAXIMUS X HERO');
+
+SELECT 
+	u.id AS user_id, u.name,
+	o.id AS order_id
+FROM 
+	users AS u
+RIGHT JOIN
+	orders AS o 
+ON
+	u.id = o.user_id;
+	
+-- 2. Выведите список товаров products и разделов catalogs, который соответствует товару.
+SELECT 
+	p.id, p.name, p.price,
+	c.id AS cat_id,
+	c.name AS catalog
+FROM
+	products AS p
+JOIN
+	catalogs AS c
+ON 
+	p.catalog_id = c.id;
+
+/* 3. (по желанию) Есть таблица рейсов flights (id, from, to) и таблица городов cities (label, name).
+Поля from, to и label содержат английские названия городов, поле name — русское.
+Выведите список рейсов (flights) с русскими названиями городов. */
+
+-- Подговка структуры и данных
+
+DROP TABLE IF EXISTS flights;
+CREATE TABLE flights (
+  id SERIAL PRIMARY KEY,
+  `from` VARCHAR(255) COMMENT 'Город отравления',
+  `to` VARCHAR(255) COMMENT 'Город прибытия'
+) COMMENT = 'Рейсы';
+
+INSERT INTO flights (`from`, `to`) VALUES
+('moscow', 'omsk'),
+('novgorod', 'kazan'),
+('irkutsk', 'moscow'),
+('omsk', 'irkutsk'),
+('moscow', 'kazan');
+
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+  id SERIAL PRIMARY KEY,
+  label VARCHAR(255) COMMENT 'Код города',
+  name VARCHAR(255) COMMENT 'Название города'
+) COMMENT = 'Словарь городов';
+
+INSERT INTO cities (label, name) VALUES
+('moscow', 'Москва'),
+('irkutsk', 'Иркутск'),
+('novgorod', 'Новгород'),
+('kazan', 'Казань'),
+('omsk', 'Омск');
+
+-- Решение
+SELECT
+	id AS flight_id,
+	(SELECT name FROM cities WHERE label = `from`) AS `from`,
+	(SELECT name FROM cities WHERE label = `to`) AS `to`
+FROM
+	flights
+ORDER BY
+	flight_id;
